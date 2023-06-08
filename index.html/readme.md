e5cbd1d190e35acf8d33817969bb50f0491257af
anubhav13singh <120702220+anubhav13singh@users.noreply.github.com>
2023-06-08 17:02:17 +0530

    Add files via upload

---
+++ index.html/readme.md
@@ -0,0 +1,53 @@
+normal  text<br>
+md means mark down
+# full stack javascript program
+
+## full stack javascript program
+### full stack javascript program
+
+#### full stack javascript program
+
+##### full stack javascript program
+
+###### full stack javascript program
+
+_batch 2.0_
+
+**Bold/strong**
+
+~~delete~~  ~~
+
+[know more](link..)
+[know more](link.. "company name")
+
+![alter text](./images/FB_IMG_1582627524938.jpg) image
+
+tables
+
+|monday|tuesday|wednesday|
+|--|--|--|
+|bio|maths|science|
+
+be `patience`
+
+```javascript
+let a=34;
+```
+
+quotes
+> just be patience ,things will come in your hand!
+
+1. Singh
+2. arpit
+1. ram
+   1. arpita
+   3. singh
+
+- anubhav
+- shyam-
+
+![](link)for batch `sheilds.io`
+ 
+
+
+
---
+++ index.html/script.js
@@ -0,0 +1,68 @@
+7
+8.6
+// numbers values
+
+("anubhav");
+('anubhav');
+// string values
+
+true =1
+false =0
+// bolean values;
+
+null 
+undefined
+// empty values
+
+// all the abbove values called preimitive data type=one singl value
+
+// array[] and object{}
+[1,2,3, "anubhav",true, 'anubhav'];
+ 0,1,2.   3  .      4      5 = index/key 
+
+
+{
+    .....key:value
+    // fname:anubhav
+}
+// this two are called as non-primitive datatype=will have more than 1 values
+
+
+// variables=place holder for vaLUES
+// var y = 68; never use var
+let x = 45;
+const z ='anubhav';
+const  y = 10094845988;
+// any fixed value which doesnt change with time
+
+let firstname ="anubhav"
+let lastname = "singh"
+const mobileno =8171738179;
+
+console.log('my first name is',/+firstname)
+console.log(lastname)
+console.log(mobileno)
+
+
+
+// operator
+// means calculation
+
+let x =20;
+let y = 40;
+let zz = x+y;
+console.log(z);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
